d5b6ee6b18c6ce9193e5dfbf50c551ee6c5bd505
Pawel Haladyj <pawel@haladyj.pl>
2024-05-07 13:03:45 +0200

    odpowiedź w formacie JSON

--- src/main/java/pl/haladyj/springaiintro/services/OpenAIServiceImpl.java
+++ src/main/java/pl/haladyj/springaiintro/services/OpenAIServiceImpl.java
@@ -1,9 +1,13 @@
 package pl.haladyj.springaiintro.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.ai.chat.ChatClient;
 import org.springframework.ai.chat.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
@@ -27,6 +31,9 @@ public class OpenAIServiceImpl implements OpenAIService{
 
     @Value("classpath:templates/get-capital-with-info-prompt.st")
     private Resource getCapitalWithInfoResource;
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Override
     public String getAnswer(String question) {
@@ -52,7 +59,16 @@ public class OpenAIServiceImpl implements OpenAIService{
         Prompt prompt = promptTemplate.create(Map.of("stateOrCountry",getCapitalRequest.stateOrCountry()));
         ChatResponse response = chatClient.call(prompt);
 
-        return new Answer(response.getResult().getOutput().getContent());
+        System.out.println(response.getResult().getOutput().getContent());
+        String responseString;
+        try{
+            JsonNode jsonNode = objectMapper.readTree(response.getResult().getOutput().getContent());
+            responseString = jsonNode.get("answer").asText();
+        } catch (JsonProcessingException e){
+            throw  new RuntimeException(e);
+        }
+
+        return new Answer(responseString);
     }
 
     @Override
